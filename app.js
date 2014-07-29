75c66230aa8fab1ac273aac31b46f93a74884972
Raghu Varma <raghuvarma@masymbol.com>
2014-07-29 12:33:10 +0530

    setup mongodb database

--- app.js
+++ app.js
@@ -2,10 +2,66 @@
 //module dependencies
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')  
+  , mongoose = require('mongoose')
+  , uniqueValidator = require('mongoose-unique-validator')
+  , bcrypt = require('bcrypt-nodejs');
+
 var app = express();
+
+/*
+ * UserSchema
+ *
+ */
+userSchema = new mongoose.Schema({
+    username:{ type: String, required: true, unique: true },
+    password:{ type: String, required: true },
+    role:{ type: String, required: true },
+    created_at:{type: Date, default: Date.now}
+});
+
+// Apply the uniqueValidator plugin to userSchema.
+userSchema.plugin(uniqueValidator);
+
+/* ==================================
+ * MongoDB connection using Mongoose
+ */
+ 
+var db = mongoose.createConnection('mongodb://localhost/navaneetha'),
+    User = db.model('users', userSchema);
+    
+
+db.on('connected', function () {
+    console.log('nananeetha database MongoDB.');
+    dbmessage = 'nananeetha mongodb connected.';
+});
+
+db.on('error', function () {
+    console.error.bind(console, 'Connection error!');
+    dbmessage = 'nananeetha MongoDB error!';
+});
+
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  var SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3002);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
